23b8797cd27d728c129944c3b3a558f647bcd0f4
octaviolafourcade <octavio.lafourcade@gmail.com>
2022-01-10 13:54:03 -0500

    [ ADD ] Base implementation of the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            Chat
+        </div>
+    )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApus-9HLfR_su9_I-xL0bsVtfVKTdEnE4",
+    authDomain: "chat-3-81678.firebaseapp.com",
+    projectId: "chat-3-81678",
+    storageBucket: "chat-3-81678.appspot.com",
+    messagingSenderId: "61367360991",
+    appId: "1:61367360991:web:15edfc81babf89f351786a",
+    measurementId: "G-Y08G6RE8TC"
+  };
+  
+  // Initialize Firebase
+  const db = firebase.firestore()
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider()
+
+  export { db, auth, provider}
